c666e082c6cb6454c5e483e2895e2de387fe2de9
lucastalmirao <lucastalmirao@gmail.com>
2024-02-16 21:54:59 -0300

    Inicial

---
+++ ServerResource/ServerResource.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Sockets;
+
+public class ServerResource
+{
+    string serverIp = "127.0.0.1";
+    int serverPort = 7173;
+    TcpClient server;
+
+    public ServerResource()
+    {
+        try
+        {
+            Console.WriteLine("Establishing connection to Main Server...");
+            server = new TcpClient(serverIp, serverPort);
+            Console.WriteLine("Connection established");
+
+            /*NetworkStream stream = client.GetStream();
+
+            // Exemplo de envio de dados para o servidor
+            string dataToSend = "Olá, servidor!";
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(dataToSend);
+            stream.Write(data, 0, data.Length);
+
+            // Receber resposta do servidor
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine("Resposta do servidor: " + response);
+
+            client.Close();*/
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error connecting on server: " + ex.Message);
+        }
+    }
+}
